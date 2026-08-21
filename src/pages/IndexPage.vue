865483e829bb54343661217bfb51f86de52a738a
Maxarnon370 <maxarmom9999@gmail.com>
2026-08-21 13:54:53 +0700

    v.2-Template-370

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,14 +1,14 @@
 <template>
   <q-page class="flex flex-center">
-    <div class="column items-center">
-      <img
-        alt="Quasar logo"
-        src="~@/assets/quasar-logo-vertical.svg"
-        style="width: 200px; height: 200px"
-      />
-
-      <q-btn class="q-mt-md" color="primary" to="/second" label="Go to Second Page" no-caps />
-    </div>
+    <q-form class="q-gutter-md" style="max-width: 600px">
+      <q-input filled label="แม็กอานนท์ *" hint="เจริญขวัญ" />
+      <q-input filled type="number" label="19" />
+      <q-toggle label="I accept the license and terms" />
+      <div>
+        <q-btn label="SUBMIT" type="submit" color="primary" />
+        <q-btn label="RESET" type="reset" color="primary" flat class="q-ml-sm" />
+      </div>
+    </q-form>
   </q-page>
 </template>
 
